72015cea7985e2ad8ec5730fb865907d4854b1e5
WanWizard <wanwizard@wanwizard.eu>
2011-06-13 18:35:33 +0200

    fixed loading views from modules

--- classes/view.php
+++ classes/view.php
@@ -25,7 +25,7 @@ class View extends \Fuel\Core\View {
 	{
 		$extension	= pathinfo($file, PATHINFO_EXTENSION);
 		$class		= \Config::get('parser.extensions.'.$extension, get_called_class());
-		$file		= pathinfo($file, PATHINFO_DIRNAME).DS.pathinfo($file, PATHINFO_FILENAME);
+		$file = ((strpos($file, DS) !== false) ? pathinfo($file, PATHINFO_DIRNAME).DS : '').pathinfo($file, PATHINFO_FILENAME);
 
 		// Class can be an array config
 		if (is_array($class))
