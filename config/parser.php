e1cffd775745748071e2ecb325194715c4d75600
Paul Boco <paulboco@cableone.net>
2011-08-22 16:03:21 -0500

    Add Twig extension configuration key

--- config/parser.php
+++ config/parser.php
@@ -70,6 +70,16 @@ return array(
 			'autoescape'           => false,
 			'optimizations'        => -1,
 		),
+		/**
+		 * Twig Extensions
+		 *
+		 * Define any Twig extensions you wish to load here.
+		 */
+		'extensions' => array(
+			'\Twig_Extensions_Extension_Text',
+			'\Twig_Extensions_Extension_Debug',
+			//'\Twig_Extensions_Extension_I18n',
+		),
 	),
 
 	// DWOO ( http://wiki.dwoo.org/ )
