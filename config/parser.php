020972eabfd30ce0122b408e35c6ce3b60961e0f
Fumito Mizuno <mizuno@php-web.net>
2011-12-04 09:12:16 +0900

    fix indent

--- config/parser.php
+++ config/parser.php
@@ -60,106 +60,106 @@ return array(
 			'tag_block'     => array('{%', '%}'),
 			'tag_comment'   => array('{#', '#}'),
 				'tag_variable'  => array('{{', '}}'),
-			),
-			'environment' => array(
-				'debug'                => false,
-				'charset'              => 'utf-8',
-				'base_template_class'  => 'Twig_Template',
-				'cache'                => APPPATH.'cache'.DS.'twig'.DS,
-				'auto_reload'          => true,
-				'strict_variables'     => false,
-				'autoescape'           => false,
-				'optimizations'        => -1,
-			),
-			'extensions' => array(
-				'Twig_Fuel_Extension'
-			),
 		),
-
-		// DWOO ( http://wiki.dwoo.org/ )
-		// ------------------------------------------------------------------------
-		'View_Dwoo' => array(
-			'include' => APPPATH.'vendor'.DS.'Dwoo'.DS.'dwooAutoload.php',
-			'auto_encode' => true,
-			'delimiters' => array('{{', '}}'),
-			'environment' => array(
-				'autoescape'       => false,
-				'nested_comments'  => false,
-				'allow_spaces'     => false,
-				'cache_dir'        => APPPATH.'cache'.DS.'dwoo'.DS,
-				'compile_dir'      => APPPATH.'cache'.DS.'dwoo'.DS.'compiled'.DS,
-				'cache_time'       => 0,
-
-				// Set what parser should do with PHP tags
-				// 1 - Encode tags | 2 - Remove tags | 3 - Allow tags
-				'allow_php_tags'   => 2,
-
-				// Which PHP functions should be accessible through Parser
-				'allow_php_func'   => array(),
-			),
+		'environment' => array(
+			'debug'                => false,
+			'charset'              => 'utf-8',
+			'base_template_class'  => 'Twig_Template',
+			'cache'                => APPPATH.'cache'.DS.'twig'.DS,
+			'auto_reload'          => true,
+			'strict_variables'     => false,
+			'autoescape'           => false,
+			'optimizations'        => -1,
 		),
-
-		// MUSTACHE ( https://github.com/bobthecow/mustache.php )
-		// ------------------------------------------------------------------------
-		'View_Mustache' => array(
-			'include' => PKGPATH.'parser'.DS.'vendor'.DS.'Mustache'.DS.'Mustache.php',
-			'auto_encode' => true,
-			'delimiters' => array('{{', '}}'),
-			'environment' => array(
-				'charset' => 'UTF-8',
-				'pragmas' => array(),
-			),
+		'extensions' => array(
+			'Twig_Fuel_Extension'
 		),
+	),
 
-		// JADE PHP ( https://github.com/everzet/jade.php )
-		// See notes in /parser/classes/view/jade.php
-		// ------------------------------------------------------------------------
-		'View_Jade' => array(
-			'include'      => APPPATH.'vendor'.DS.'Jade'.DS.'autoload.php.dist',
-			'auto_encode'  => true,
-			'cache_dir'    => APPPATH.'cache'.DS.'jade'.DS,
-		),
+	// DWOO ( http://wiki.dwoo.org/ )
+	// ------------------------------------------------------------------------
+	'View_Dwoo' => array(
+		'include' => APPPATH.'vendor'.DS.'Dwoo'.DS.'dwooAutoload.php',
+		'auto_encode' => true,
+		'delimiters' => array('{{', '}}'),
+		'environment' => array(
+			'autoescape'       => false,
+			'nested_comments'  => false,
+			'allow_spaces'     => false,
+			'cache_dir'        => APPPATH.'cache'.DS.'dwoo'.DS,
+			'compile_dir'      => APPPATH.'cache'.DS.'dwoo'.DS.'compiled'.DS,
+			'cache_time'       => 0,
 
-		// HAML / PHAMLP ( http://code.google.com/p/phamlp/ )
-		// ------------------------------------------------------------------------
-		'View_Haml'   => array(
-			'include'      => APPPATH.'vendor'.DS.'Phamlp'.DS.'haml'.DS.'HamlParser.php',
-			'auto_encode'  => true,
-			'cache_dir'    => APPPATH.'cache'.DS.'haml'.DS,
-		),
+			// Set what parser should do with PHP tags
+			// 1 - Encode tags | 2 - Remove tags | 3 - Allow tags
+			'allow_php_tags'   => 2,
 
-		// SMARTY ( http://www.smarty.net/documentation )
-		// ------------------------------------------------------------------------
-		'View_Smarty'   => array(
-			'include'       => APPPATH.'vendor'.DS.'Smarty'.DS.'libs'.DS.'Smarty.class.php',
-			'auto_encode' => true,
-			'delimiters'    => array('{', '}'),
-			'environment'   => array(
-				'compile_dir'       => APPPATH.'tmp'.DS.'Smarty'.DS.'templates_c'.DS,
-				'config_dir'        => APPPATH.'tmp'.DS.'Smarty'.DS.'configs'.DS,
-				'cache_dir'         => APPPATH.'cache'.DS.'Smarty'.DS,
-				'caching'           => false,
-				'cache_lifetime'    => 0,
-				'force_compile'     => false,
-				'compile_check'     => true,
-				'debugging'         => false,
-				'autoload_filters'  => array(),
-				'default_modifiers' => array(),
-			),
+			// Which PHP functions should be accessible through Parser
+			'allow_php_func'   => array(),
 		),
+	),
 
-		// Phptal ( http://phptal.org/manual/en/ )
-		// ------------------------------------------------------------------------
-		'View_Phptal'   => array(
-			'include'   => APPPATH.'vendor'.DS.'PHPTAL'.DS.'PHPTAL.php',
-			'auto_encode' => true,
-			'cache_dir' => APPPATH.'cache'.DS.'PHPTAL'.DS,
-			'cache_lifetime' => 0,
-			'encoding' => 'UTF-8',
-			'output_mode' => 'PHPTAL::XHTML',
-			'template_repository' => '',
-			'force_reparse' => false,
+	// MUSTACHE ( https://github.com/bobthecow/mustache.php )
+	// ------------------------------------------------------------------------
+	'View_Mustache' => array(
+		'include' => PKGPATH.'parser'.DS.'vendor'.DS.'Mustache'.DS.'Mustache.php',
+		'auto_encode' => true,
+		'delimiters' => array('{{', '}}'),
+		'environment' => array(
+			'charset' => 'UTF-8',
+			'pragmas' => array(),
 		),
-	);
+	),
+
+	// JADE PHP ( https://github.com/everzet/jade.php )
+	// See notes in /parser/classes/view/jade.php
+	// ------------------------------------------------------------------------
+	'View_Jade' => array(
+		'include'      => APPPATH.'vendor'.DS.'Jade'.DS.'autoload.php.dist',
+		'auto_encode'  => true,
+		'cache_dir'    => APPPATH.'cache'.DS.'jade'.DS,
+	),
+
+	// HAML / PHAMLP ( http://code.google.com/p/phamlp/ )
+	// ------------------------------------------------------------------------
+	'View_Haml'   => array(
+		'include'      => APPPATH.'vendor'.DS.'Phamlp'.DS.'haml'.DS.'HamlParser.php',
+		'auto_encode'  => true,
+		'cache_dir'    => APPPATH.'cache'.DS.'haml'.DS,
+	),
+
+	// SMARTY ( http://www.smarty.net/documentation )
+	// ------------------------------------------------------------------------
+	'View_Smarty'   => array(
+		'include'       => APPPATH.'vendor'.DS.'Smarty'.DS.'libs'.DS.'Smarty.class.php',
+		'auto_encode' => true,
+		'delimiters'    => array('{', '}'),
+		'environment'   => array(
+			'compile_dir'       => APPPATH.'tmp'.DS.'Smarty'.DS.'templates_c'.DS,
+			'config_dir'        => APPPATH.'tmp'.DS.'Smarty'.DS.'configs'.DS,
+			'cache_dir'         => APPPATH.'cache'.DS.'Smarty'.DS,
+			'caching'           => false,
+			'cache_lifetime'    => 0,
+			'force_compile'     => false,
+			'compile_check'     => true,
+			'debugging'         => false,
+			'autoload_filters'  => array(),
+			'default_modifiers' => array(),
+		),
+	),
+
+	// Phptal ( http://phptal.org/manual/en/ )
+	// ------------------------------------------------------------------------
+	'View_Phptal'   => array(
+		'include'   => APPPATH.'vendor'.DS.'PHPTAL'.DS.'PHPTAL.php',
+		'auto_encode' => true,
+		'cache_dir' => APPPATH.'cache'.DS.'PHPTAL'.DS,
+		'cache_lifetime' => 0,
+		'encoding' => 'UTF-8',
+		'output_mode' => 'PHPTAL::XHTML',
+		'template_repository' => '',
+		'force_reparse' => false,
+	),
+);
 
 				// end of file parser.php
