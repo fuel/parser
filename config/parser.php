50d794c8d8dbf7587d07bbe0f77240ca8e843f64
Thomas David Baker <bakert@gmail.com>
2012-07-01 14:55:32 +0100

    Use the official Smarty API way of adding a plugins directory.
    Use an array as the default value so that (a) we don't point at
    directories that don't exist and (b) we hint to the client
    programmer that more than one directory can be added.
    
    This way you can add more than one plugins dir, too, giving you
    more flexibility.

--- config/parser.php
+++ config/parser.php
@@ -138,7 +138,7 @@ return array(
 			'compile_dir'       => APPPATH.'tmp'.DS.'Smarty'.DS.'templates_c'.DS,
 			'config_dir'        => APPPATH.'tmp'.DS.'Smarty'.DS.'configs'.DS,
 			'cache_dir'         => APPPATH.'cache'.DS.'Smarty'.DS,
-			'plugins_dir'       => APPPATH.'smarty'.DS,
+			'plugins_dir'       => array(),
 			'caching'           => false,
 			'cache_lifetime'    => 0,
 			'force_compile'     => false,
