bb264bd14f5dec985f13791ed21ab8732f0890f5
WanWizard <wanwizard@wanwizard.eu>
2013-01-19 00:41:21 +0100

    bumped the copyright to 2013

--- classes/view.php
+++ classes/view.php
@@ -5,10 +5,10 @@
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
  * @package    Fuel
- * @version    1.0
+ * @version    1.5
  * @author     Fuel Development Team
  * @license    MIT License
- * @copyright  2010 - 2012 Fuel Development Team
+ * @copyright  2010 - 2013 Fuel Development Team
  * @link       http://fuelphp.com
  */
 
@@ -61,7 +61,7 @@ class View extends \Fuel\Core\View
 
 			$class = \Config::get('parser.extensions.'.$extension, null);
 		}
-		
+
 		if ($class === null)
 		{
 			$class = get_called_class();
@@ -102,7 +102,7 @@ class View extends \Fuel\Core\View
 		{
 			// Set extension when given
 			$extension and $view->extension = $extension;
-			
+
 			// Load the view file
 			$view->set_filename($file);
 		}
