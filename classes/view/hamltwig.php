f7bc5a3e112460384b4c6e475fc354b386afba41
WanWizard <wanwizard@wanwizard.eu>
2013-04-21 14:45:42 +0200

    bumped the docblock version to 1.6

--- classes/view/hamltwig.php
+++ classes/view/hamltwig.php
@@ -5,7 +5,7 @@
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
  * @package    Fuel
- * @version    1.5
+ * @version    1.6
  * @author     Fuel Development Team
  * @license    MIT License
  * @copyright  2010 - 2013 Fuel Development Team
@@ -21,34 +21,34 @@ use Twig_Lexer;
 use MtHaml;
 
 class View_HamlTwig extends View_Twig {
-	
+
 	protected static $_environment;
-	
+
 	/**
 	 * @access public
 	 * @static
 	 * @return void
 	 */
 	public static function _init()
-	{		
+	{
 		// Include View_HamlTwig file(s) defined in config.
 		$includes = \Config::get('parser.View_Twig.include');
-		
+
 		foreach ((array)$includes as $include)
 		{
 			require $include;
 			static::$loaded_files[$include] = true;
 		}
-		
+
 		parent::_init();
-		
+
 		MtHaml\Autoloader::register();
-		
+
 	}
-	
+
 	/**
 	 * We Override the parser Loader here
-	 * 
+	 *
 	 * @access protected
 	 * @static
 	 */
@@ -65,7 +65,7 @@ class View_HamlTwig extends View_Twig {
 		// Twig Loader
 		$views_paths = \Config::get('parser.View_Twig.views_paths', array(APPPATH . 'views'));
 		array_unshift($views_paths, pathinfo($file, PATHINFO_DIRNAME));
-				
+
 		if ( ! empty($global_data))
 		{
 			foreach ($global_data as $key => $value)
@@ -78,11 +78,11 @@ class View_HamlTwig extends View_Twig {
 			// Init the parser if you have no global data
 			static::parser();
 		}
-		
+
 		// Set the HtHaml Twig loader
 		$filesyst = new Twig_Loader_Filesystem($views_paths);
 		static::$_parser_loader = new MtHaml\Support\Twig\Loader(static::$_environment, $filesyst);
-		
+
 		$twig_lexer = new Twig_Lexer(static::$_parser, static::$_twig_lexer_conf);
 		static::$_parser->setLexer($twig_lexer);
 		//\Debug::dump(static::parser()); exit();
@@ -97,7 +97,7 @@ class View_HamlTwig extends View_Twig {
 			throw $e;
 		}
 	}
-	
+
 	/**
 	 * @access public
 	 * @static
@@ -108,7 +108,7 @@ class View_HamlTwig extends View_Twig {
 		if (empty(static::$_parser))
 		{
 			parent::parser();
-			
+
 			// Register Haml twig supports
 			static::$_parser->addExtension(new MtHaml\Support\Twig\Extension());
 				// Store MtHaml environment
--- config/parser.php
+++ config/parser.php
@@ -1,14 +1,15 @@
 <?php
 /**
+ * Fuel
+ *
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
  * @package    Fuel
- * @version    1.5
+ * @version    1.6
  * @author     Fuel Development Team
  * @license    MIT License
  * @copyright  2010 - 2013 Fuel Development Team
  * @link       http://fuelphp.com
- */
 
 /**
  * NOTICE:
@@ -75,7 +76,7 @@ return array(
 			'Twig_Fuel_Extension'
 		),
 	),
-	
+
 	// HamlTwig with MtHaml https://github.com/arnaud-lb/MtHaml
 	// Twig configuration is grabbed from 'View_Twig' config key
 	// Packagist url: https://packagist.org/packages/mthaml/mthaml
